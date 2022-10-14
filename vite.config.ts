7db98323a4218c026182008885a471fedb10b7b5
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2022-10-14 15:09:59 +0200

    Configure vite to include tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    WindiCSS({
+      root: __dirname,
+      configFiles: [resolve(__dirname, 'windi.config.ts')],
+      scan: {
+        dirs: [
+          'app/_layouts', 'app/_includes', 'app/_posts', 'app/_assets', 'app/community', 'app/cookie-policy',
+          'app/docs', 'app/enterprise', 'app/install', 'app/policies', 'app/privacy', 'app/resources',
+          'app/servicemeshcon', 'terms', 'app/use-cases'
+        ],
+      },
+    }),
   ],
 })
