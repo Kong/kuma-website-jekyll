978b56c9f3a2ced0f0494fc1da871183f2dc2e61
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2022-10-18 15:36:26 +0200

    Version Selector is not always displayed

--- app/_assets/javascripts/version_selector.js
+++ app/_assets/javascripts/version_selector.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   const dropdown = document.getElementById('version-selector');
 
-  dropdown.addEventListener('change', (event) => {
-    event.stopPropagation();
+  if (dropdown !== null) {
+    dropdown.addEventListener('change', (event) => {
+      event.stopPropagation();
 
-    let currentPath = window.location.pathname;
-    let segments = currentPath.split('/');
-    segments[2] = event.target.value;
+      let currentPath = window.location.pathname;
+      let segments = currentPath.split('/');
+      segments[2] = event.target.value;
 
-    window.location.pathname = segments.join('/');
-  });
+      window.location.pathname = segments.join('/');
+    });
+  }
 });
